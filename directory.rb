7e1548464e2ab23e0d5a1ea30db7fcec5758ea82
Danny <danielajohn1406@gmail.com>
2023-03-08 18:43:17 +0000

    Student Directory - ruby_file

---
+++ directory.rb
@@ -0,0 +1,19 @@
+# The names of the students
+puts "The students of Villians Academy"
+puts "----------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "ALex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffery Baratheon"
+puts "Norman Bates"
+# Final line
+print "Overall, we have "
+# Total number of students
+print 11
+puts " great students"
