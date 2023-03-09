b2a041d61cd6f743e03fff7578b8129a65a43bc5
Danny <danielajohn1406@gmail.com>
2023-03-09 12:38:26 +0000

    Created methods for each section

--- directory.rb
+++ directory.rb
@@ -13,9 +13,21 @@ students = [
   "Norman Bates"
 ]
 
+def print_header
+  puts "The students of Villians Academy"
+  puts "----------------"
+end
 
-puts "The students of Villians Academy"
-puts "----------------"
-students.each { |student| puts student }
-# Final line
-puts "Overall, we have #{students.count} great students."
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students."
+end
+
+print_header
+print(students)
+print_footer(students)
